3cd0f6663b8714dfa37e168bb9fdad069151b157
Sarah <115491865+Sarah-Emmons@users.noreply.github.com>
2022-11-30 21:20:34 -0700

    Images in src folder
    Moved image folder into source folder.
    Updated CSS and HTML  to reflect.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+let apiKey = "3a40dbd3f099ea4205eb9b6fb6f44ot3";
+let units = "metric";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=${apiKey}&units=${units}`;
+console.log(apiUrl);
